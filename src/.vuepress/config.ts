f3705ec6ee4f6c7abda0227133285c507742a833
12306hujunjie <545512690@qq.com>
2022-11-08 11:48:27 +0800

    upgrade vuepress version

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,62 @@
+
+// .vuepress/config.ts
+
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+import recoTheme from 'vuepress-theme-reco'
+
+export default defineUserConfig({
+  title: 'database of memory',
+  description: 'study anything and life record',
+  theme: recoTheme({
+    style: '@vuepress-reco/style-default',
+    logo: '/logo.png',
+    author: "hujunjie",
+    authorAvatar: "/avatar.jpeg",
+    docsRepo: 'https://github.com/12306hujunjie/blog',
+    docsBranch: 'master',
+    docsDir: 'src',
+    lastUpdatedText: '最新更新于',
+    autoSetBlogCategories: true,         // 自动设置分类
+    autoAddCategoryToNavbar: true,  // 自动将首页、分类和标签添加至头部导航条
+    catalogTitle: '目录',
+    commentConfig: {
+      type: 'valine',
+      options: {
+        "appId": "9dl7DeiOQ2DmQoIcai3UC6dR-gzGzoHsz",
+        "appKey": "kisH4g4RCIHn0064CXERJktf",
+        placeholder: '填写邮箱可以收到回复提醒哦！',
+        verify: true, // 验证码服务
+        // notify: true,
+        recordIP: true,
+        // hideComments: true // 隐藏评论
+      },
+    },
+    // series 为原 sidebar
+    series: {
+      '/docs/theme-reco/': [
+        {
+          text: 'module one',
+          children: ['home', 'theme']
+        },
+        {
+          text: 'module two',
+          children: ['api', 'plugin']
+        }
+      ]
+    },
+    navbar:
+        [
+          // { text: 'Home', link: '/' },
+          // { text: 'Categories', link: '/categories/reco/1/' },
+          // { text: 'Tags', link: '/tags/tag1/1/' },
+          { text: 'Docs',
+            children: [
+              { text: 'vuepress-reco', link: '/docs/theme-reco/theme' },
+              { text: 'vuepress-theme-reco', link: '/blogs/other/guide' }
+            ]
+          },
+        ],
+  }),
+  // debug: true,
+})
---
+++ src/index.md
@@ -0,0 +1,38 @@
+---
+home: true
+modules:
+- Banner
+- Blog
+- MdContent
+- Footer
+banner: # banner 模块的配置
+  heroText: hujunjie's blog
+  tagline: study anything and life record.
+  bgImage: /bg.svg
+  bgImageStyle:
+    height: 450px
+    width: 100%
+bannerBrand: 
+  heroImage: /logo.png 
+  heroImageStyle:
+    maxWidth: '200px'
+    width: '100%'
+  display: block
+  margin: '0 auto 2rem'
+  borderRadius: '1rem'
+  bgImage: '/bg.svg'
+  heroText: hujunjie's blog
+  tagline: 花落的地方即是风景
+blog: # blog 模块的配置
+  socialLinks: # 社交 icon 请到 [Xions](https://www.xicons.org/#/zh-CN) 页面的 tabler 下获取，复制名称即可
+    - { icon: 'BrandGithub', link: 'https://github.com/12306hujunjie' }
+footer: # 底部模块的配置
+  record: 沪ICP备 2022003331 号
+  recordLink: https://beian.miit.gov.cn/
+  cyberSecurityRecord: 公安备案文案
+  cyberSecurityLink: 公安备案地址
+  startYear: 2021
+buttons:
+  - { text: Guide, link: '/docs/guide/introduce' }
+  - { text: Default Style, link: '/docs/style-default-api/introduce', type: 'plain' }
+---
